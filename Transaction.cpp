c8217d2d7418ea73dd5b26fd5a943f2207e180da
JesusHigueraSKY <jesus.higuera.a8@gmail.com>
2024-11-19 12:06:15 -0600

    Add files via upload

---
+++ Transaction.cpp
@@ -0,0 +1,14 @@
+#include "classtransaction.h"
+
+Transaction::Transaction(string_type, double_amount){
+    type = _type
+    amount = _amount
+}
+
+string Transaction::getType(){
+    return type;
+}
+
+double Transaction::getAmount(){
+    return amount;
+}
