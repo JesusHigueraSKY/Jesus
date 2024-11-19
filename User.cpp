2aff20a2497c5eb181ed28009b11501b5be39035
JesusHigueraSKY <jesus.higuera.a8@gmail.com>
2024-11-19 12:05:58 -0600

    Add files via upload

---
+++ User.cpp
@@ -0,0 +1,14 @@
+#include "classuser.h"
+
+Usuario::User(string_name, string_age) {
+    name = _name;
+    age = _age;
+}
+
+string User::getName(){
+    return name;
+}
+
+string User::getAge(){
+    return age;
+}
